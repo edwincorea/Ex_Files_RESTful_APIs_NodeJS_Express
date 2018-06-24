e084f10c8c150cd4c24b94bef0b46cb77fa6a414
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-24 16:47:50 -0600

    Static files serving

--- CRM/index.js
+++ CRM/index.js
@@ -16,6 +16,9 @@ app.use(bodyParser.json());
 
 routes(app);
 
+// serving static files
+app.use(express.static('public'));
+
 app.get('/', (req, res) => 
     res.send(`Node and Express server is running on port ${PORT}`)
 );
