1a409f00269874fc4df494593d44e6ce3784ec1b
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-24 15:52:49 -0600

    Create all items GET endpoint

--- CRM/src/controllers/crmController.js
+++ CRM/src/controllers/crmController.js
@@ -14,3 +14,11 @@ export const addNewContact = (req, res) => {
     });
 };
 
+export const getContacts = (req, res) => {
+    Contact.find({}, (err, contacts) => {
+        if (err){
+            res.send(err);
+        }
+        res.json(contacts);
+    });
+};
